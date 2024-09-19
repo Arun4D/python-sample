0eb8707041110b1d8161dc1a9016b0ba2242096f
arun <arun4.duraisamy@gmail.com>
2024-09-19 09:03:20 +0530

    Added parse logic

--- migration/migration/convert_to_restnsx.py
+++ migration/migration/convert_to_restnsx.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import re
 
 from . import helpers
 
@@ -37,6 +38,10 @@ FIELD_MAPPING = {
     'Port': 'Port Criteria'
 }
 
+INPUT_EXCEL_COLUMNS_TO_READ = ['Source', 'Destination', 'Port'] 
+INPUT_EXCEL_SECURITY_COLUMNS = ['Source', 'Destination'] 
+INPUT_SERVICE_COLUMNS = ['Port'] 
+
 # Step 2: Read Excel and store data into class objects
 def read_excel_into_objects(file_path, columns):
     # Read the specific columns from Excel
@@ -46,10 +51,57 @@ def read_excel_into_objects(file_path, columns):
     data_objects = []
     for _, row in df.iterrows():
         # Create an instance of InputExcelData for each row
-        data_obj = InputExcelData(row[columns[0]], row[columns[1]], row[columns[2]])
+        column1 = convert_to_nst_format(row[columns[0]], columns[0])
+        column2 = convert_to_nst_format(row[columns[1]], columns[1])
+        column3 = convert_to_nst_format(row[columns[2]], columns[2])
+        data_obj = InputExcelData(column1, column2, column3)
         data_objects.append(data_obj)
     
     return data_objects
+
+def convert_to_nst_format(string_input, column_name):
+    result = parse_key_value_string(string_input)
+    
+    prefix= ""
+    if column_name in INPUT_EXCEL_SECURITY_COLUMNS:
+        prefix= "SecurityGroup"
+    elif column_name in INPUT_SERVICE_COLUMNS:
+        prefix= "Service"
+
+    result = parse_to_pipe_seperated(result, prefix)
+    
+    return result
+
+
+def parse_key_value_string(s):
+    # Regular expression to match 'key : value' patterns, with spaces around the colon
+    pattern = r'(\S+)\s*:\s*(\S+)'
+    
+    # Find all key-value pairs
+    matches = re.findall(pattern, s)
+    
+    # Convert the matches into a dictionary
+    result = {key: value for key, value in matches}
+
+    return result
+    
+
+def parse_to_pipe_seperated(input_dict, group_prefix):
+    result = ""
+    keys = ""
+    values = ""
+    for key, value in input_dict.items():
+        keys = keys + group_prefix + ":"+key + "|" 
+        values = values + group_prefix + ":"+value + "|" 
+    result = keys+values
+    if bool(result and result.strip()):
+        return result[:-1]
+
+    return result
+
+    print(f"result  {result}")
+        
+    return result
 
 # Step 3: Convert class objects to CSV
 def write_objects_to_csv(data_objects, output_csv_path):
@@ -78,11 +130,8 @@ if __name__ == "__main__":
     input_excel_path = helpers.get_input_excel_name()
     output_csv_path =  helpers.get_output_csv_name()
 
-    input_excel_columns_to_read = ['Source', 'Destination', 'Port']  # Replace with your actual column names
-
-
     # Read data from Excel into class objects
-    data_objects = read_excel_into_objects(input_excel_path, input_excel_columns_to_read)
+    data_objects = read_excel_into_objects(input_excel_path, INPUT_EXCEL_COLUMNS_TO_READ)
     
     # Convert the class objects into CSV format
     write_objects_to_csv(data_objects, output_csv_path)
